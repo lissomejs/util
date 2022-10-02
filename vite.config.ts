0ba3c0d90fc230af6626a6dbaf277e8f74af891b
haozhenjia <haozhenjia@cbim.org.cn>
2022-10-02 21:26:58 +0800

    build: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [
+        dts()
+    ],
     build: {
         outDir: 'lib',
         lib: {

81c0d8b99fd38fdb4b2a68938aecee09dd7f39bf
haozhenjia <cheeka@163.com>
2023-06-08 18:54:55 +0800

    feat: 完善构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        dts()
-    ],
+    plugins: [dts()],
     build: {
-        outDir: 'lib',
+        outDir: "lib",
         lib: {
-            entry: path.resolve(__dirname, 'src/index.ts'),
-            name: 'LissomeUtil',
+            entry: "src/index.ts",
+            formats: ["es", "umd", "cjs"],
+            name: "LissomeUtil",
         },
     },
-})
+});
